4ca8472ad7e1522982338890cf10192aa29b13b6
jibundeyare <contact@jibundeyare.com>
2021-07-07 16:45:00 +0200

    Premier commit

--- front-page.php
+++ front-page.php
@@ -17,7 +17,27 @@ endif;
 
 // affichez la liste des posts
 $args = array(
+    // Sélection de tous les posts
     'post_type' => 'post',
 );
+
+// Exécution de la requête WP_Query
+$query = new WP_Query( $args );
+
+// Affichage du résultat de la requête WP_Query avec la boucle
+if ( $query->have_posts() ):
+    while ( $query->have_posts() ):
+        $query->the_post();
+        ?>
+        <article>
+            <h1><?php the_title(); ?></h1>
+            <div><?php the_content(); ?></div>
+        </article>
+        <?php
+    endwhile;
+endif;
+
+// Restauration des paramètres originaux de la requête de l'utilisateur
+wp_reset_postdata();
 
 get_footer();
